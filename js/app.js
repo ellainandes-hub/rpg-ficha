276c9bc0b2284284215a2e39a6a215421b744525
norman <ellai.nandes@gmail.com>
2026-03-21 14:54:10 -0300

    corrige app.js para api online

--- js/app.js
+++ js/app.js
@@ -1,12 +1,8 @@
-const API = "http://localhost:3000"
+const API = "https://rpg-ficha-api.onrender.com";
 
-
-
-// LOGIN
 async function login() {
-
-    const usuario = document.getElementById("usuario").value
-    const senha = document.getElementById("senha").value
+    const usuario = document.getElementById("usuario").value;
+    const senha = document.getElementById("senha").value;
 
     const resposta = await fetch(`${API}/auth/login`, {
         method: "POST",
@@ -17,34 +13,28 @@ async function login() {
             usuario,
             senha
         })
-    })
+    });
 
-    const dados = await resposta.json()
+    const dados = await resposta.json();
 
     if (dados.erro) {
-        alert(dados.erro)
-        return
+        alert(dados.erro);
+        return;
     }
 
-    localStorage.setItem("token", dados.token)
-    localStorage.setItem("tipo", dados.tipo)
+    localStorage.setItem("token", dados.token);
+    localStorage.setItem("tipo", dados.tipo);
 
     if (dados.tipo === "admin") {
-        window.location.href = "pages/admin.html"
+        window.location.href = "pages/admin.html";
     } else {
-        window.location.href = "pages/personagens.html"
+        window.location.href = "pages/personagens.html";
     }
-
 }
 
-
-
-
-// CRIAR CONTA
 async function criarConta() {
-
-    const usuario = document.getElementById("novoUsuario").value
-    const senha = document.getElementById("novaSenha").value
+    const usuario = document.getElementById("novoUsuario").value;
+    const senha = document.getElementById("novaSenha").value;
 
     const resposta = await fetch(`${API}/auth/register`, {
         method: "POST",
@@ -55,15 +45,14 @@ async function criarConta() {
             usuario,
             senha
         })
-    })
+    });
 
-    const dados = await resposta.json()
+    const dados = await resposta.json();
 
     if (dados.erro) {
-        alert(dados.erro)
-        return
+        alert(dados.erro);
+        return;
     }
 
-    alert("Conta criada com sucesso!")
-
+    alert("Conta criada com sucesso!");
 }
